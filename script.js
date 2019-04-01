0956d43803e1c37fe5b4962d3a6c53168d977c25
Naveen Kumar Pulikonda <naveenpulikondaaiee@gmail.com>
2019-04-01 17:43:57 +0530

    first upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function() {
+	
+	$('.numbers').counterUp({
+
+		delay:10,
+		time:1000
+	});
+});
+
+
+$(document).ready(function(){
+    'use strict';
+
+    new WOW().init();
+})
